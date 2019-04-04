65e96033bc9e3e3a4f796f740ab004db5d598db7
_Epyon <TheEpyonProject@gmail.com>
2019-04-04 07:18:10 -0400

    whatamidoing
    trying to do heroku stuff

---
+++ multiplayer-game/js/MainMenu.js
@@ -0,0 +1,161 @@
+"use strict";
+
+GameStates.makeMainMenu = function( game, shared ) {
+
+	var music = null;
+	var statsButton = null;
+    var clockCircle = null;
+	var clockMinute = 0;
+	var clockSecond = 0;
+	var clockMinuteLine = null;
+	var clockSecondLine = null;
+	
+	
+	var newGameButton = null;
+	var howtoButton = null;
+	var closeButton = null;
+	
+	var infoBox;
+	var infoText;
+	
+	var openTween;
+	var closeTween;
+	
+    function startGame(pointer) {
+
+        //	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+        // music.stop();
+
+        //	And start the actual game
+        game.state.start('Game');
+
+    }
+	
+	function drawClock() {
+			clockCircle = game.add.graphics(0, 0);
+			clockCircle.beginFill(0xdff6f5, 1);
+			clockCircle.drawCircle(200, 500, 400);
+			clockCircle.endFill();
+			clockCircle.beginFill(0xffffff, 1);
+			clockCircle.drawCircle(200, 500, 380);
+			clockCircle.endFill();
+			
+			
+			
+			// Clock Minute Line
+			clockMinute = 0;
+			clockMinuteLine = game.add.graphics(0, 0);
+			clockMinuteLine.beginFill(0x000000);
+			clockMinuteLine.lineStyle(10, 0x000000, 0);
+			clockMinuteLine.lineTo(200, 500);
+			clockMinuteLine.moveTo(200 - (Math.sin(clockMinute) * 100), 500 - (Math.cos(clockMinute) * 100));
+			clockMinuteLine.endFill();
+			
+
+			// Clock Second Line
+			clockSecond = 0;
+			clockSecondLine = game.add.graphics(0, 0);
+			clockSecondLine.beginFill(0x000000);
+			clockSecondLine.lineStyle(8, 0x000000, 0);
+			clockSecondLine.lineTo(200, 500);
+			clockSecondLine.moveTo(200 - (Math.sin(clockSecond) * 140), 500 - (Math.cos(clockSecond) * 140));
+			clockSecondLine.endFill();
+			
+	}
+	
+	function updateClock() {
+		
+		clockSecond++;
+		if (clockSecond > 59) {
+			clockSecond = 0;
+			clockMinute++;
+			
+			if (clockMinute > 59) {
+				clockMinute = 0;
+			}
+		}
+		clockMinuteLine.clear();
+		clockMinuteLine.lineStyle(10, 0x000000, 1);
+		clockMinuteLine.lineTo(200, 500);
+		clockMinuteLine.moveTo(200 + (Math.sin(clockMinute / 10) * 100), 500 - (Math.cos(clockMinute / 10) * 100));
+		
+		clockSecondLine.clear();
+		clockSecondLine.lineStyle(6, 0x000000, 1);
+		clockSecondLine.lineTo(200, 500);
+		clockSecondLine.moveTo(200 + (Math.sin(clockSecond / 10) * 140), 500 - (Math.cos(clockSecond / 10) * 140));
+		
+		
+//console.log("Time is: " + clockMinute + " minutes and " + clockSecond + " seconds.");
+		//console.log("Angles are: : Minute " + Math.sin(clockMinute * 6) + "  and Second " + Math.sin(clockSecond * 6) + ".");
+	}
+    
+	
+	function openInfo() {
+		openTween = game.add.tween(infoBox.scale).to({x: 160, y: 200}, 1000, Phaser.Easing.Exponential.Out, true);
+		openTween.onComplete.add(showInfo, this);
+		infoBox.alpha = 1;
+	}
+	
+	function showInfo() {
+		
+		infoText.alpha = 1;
+		closeButton.alpha = 1;
+		closeButton.inputEnabled = true;
+		
+		newGameButton.inputEnabled = false;
+		howtoButton.inputEnabled = false;
+	}
+	
+	function closeInfo(){
+		if (openTween && openTween.isRunning || infoBox.scale == 0) {
+			return;
+		}
+
+		closeTween = game.add.tween(infoBox.scale).to({x: 0, y: 0}, 500, Phaser.Easing.Exponential.In, true);
+		infoBox.apha = 0;
+		infoText.alpha = 0;
+		closeButton.alpha = 0;
+		closeButton.inputEnabled = false;
+		newGameButton.inputEnabled = true;
+		howtoButton.inputEnabled = true;
+	}
+	
+    return {
+    
+        create: function () {
+    
+            //music = game.add.audio('titleMusic');
+            //music.play();
+    		
+			game.stage.backgroundColor = "#696969";
+	
+			drawClock();
+			game.time.events.loop(Phaser.Timer.SECOND, updateClock, this);
+			
+			newGameButton = game.add.button ( 525, 50, "newGameButton", startGame, null, "over", "out", "down");
+			howtoButton = game.add.button( 525, 100, "howtoButton", openInfo, null, "over", "out", "down");
+			
+			infoBox = game.add.sprite(0, 0, "infoBox");
+			infoBox.alpha = 0;
+			infoText = game.add.text(25, 50, "\
+			Stop the end of the world as we know it!\n\
+			Collect materials to create the catalyst to\n\
+			stop the portal from opening.  Obtain clues to\n\
+			find the correct combination of three materials\n\
+			before the countdown reaches zero!\
+			", { font: "32px Arial", fill: "#000", align: "left" });
+			infoText.anchor.set(0);
+			infoText.alpha = 0;
+			closeButton = game.add.button( 700, 50, "closeButton", closeInfo, null, "over", "out", "down");
+			closeButton.alpha = 0;
+			
+        },
+    
+        update: function () {
+    
+            //	Do some nice funky main menu effect here
+    
+        }
+        
+    };
+};
---
+++ multiplayer-game/js/Preloader.js
@@ -0,0 +1,85 @@
+"use strict";
+
+GameStates.makePreloader = function( game ) {
+
+	var background = null;
+	var preloadBar = null;
+
+	var ready = false;
+
+    return {
+    
+        preload: function () {
+    
+            //	These are the assets we loaded in Boot.js
+            //	A nice sparkly background and a loading progress bar
+            //preloadBar = game.add.sprite(300, 400, 'preloaderBar');
+    
+            //	This sets the preloadBar sprite as a loader sprite.
+            //	What that does is automatically crop the sprite from 0 to full-width
+            //	as the files below are loaded in.
+            //game.load.setPreloadSprite(preloadBar);
+    
+            //	Here we load the rest of the assets our game needs.
+            //	As this is just a Project Template I've not provided these assets, swap them for your own.
+            //	+ lots of other required assets here
+            //game.load.image( 'logo', 'assets/phaser.png' );
+			game.load.atlas("newGameButton", "assets/newGameButton.png", "assets/newGameButton.json");
+			game.load.atlas('backArrow', 'assets/backArrow.png', 'assets/backArrow.json');
+			game.load.atlas("rollButton", "assets/rollButton.png", "assets/rollButton.json");
+			game.load.atlas("howtoButton", "assets/howtoButton.png", "assets/howtoButton.json");
+			game.load.atlas("helpButton", "assets/helpButton.png", "assets/helpButton.json");
+			game.load.atlas("cluesButton", "assets/cluesButton.png", "assets/cluesButton.json");
+			game.load.atlas("questionButton", "assets/questionButton.png", "assets/questionButton.json");
+			game.load.atlas("closeButton", "assets/closeButton.png", "assets/closeButton.json");
+			
+			game.load.image("infoBox", "assets/infoBox.png");
+			
+			
+			game.load.image("red", "assets/red.png", 32, 32);
+			game.load.image("blue", "assets/blue.png", 32, 32);
+			game.load.image("yellow", "assets/yellow.png", 32, 32);
+			game.load.image("green", "assets/green.png", 32, 32);
+			game.load.image("cyan", "assets/cyan.png", 32, 32);
+			game.load.image("purple", "assets/purple.png", 32, 32);
+			game.load.tilemap("map_demo", "assets/demo.json", null, Phaser.Tilemap.TILED_JSON);
+			game.load.image("maptiles", "assets/map_tiles.png");
+			game.load.image("maparrows", "assets/map_arrows.png");
+			
+			
+			game.load.image("materialTome", "assets/materialTome.png");
+			game.load.image("materialRing", "assets/materialRing.png");
+			game.load.image("materialTwig", "assets/materialTwig.png");
+			game.load.image("materialThread", "assets/materialThread.png");
+			game.load.image("materialDagger", "assets/materialDagger.png");
+			game.load.image("materialCandle", "assets/materialCandle.png");
+        },
+    
+        create: function () {
+    
+            //	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
+			// preloadBar.cropEnabled = false;
+    
+        },
+    
+        update: function () {
+    
+            //	You don't actually need to do this, but I find it gives a much smoother game experience.
+            //	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
+            //	You can jump right into the menu if you want and still play the music, but you'll have a few
+            //	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
+            //	it's best to wait for it to decode here first, then carry on.
+            
+            //	If you don't have any music in your game then put the game.state.start line into the create function and delete
+            //	the update function completely.
+            
+            //if (game.cache.isSoundDecoded('titleMusic') && ready == false)
+            //{
+            //  ready = true;
+                game.state.start('MainMenu');
+            //}
+    
+        }
+    
+    };
+};
